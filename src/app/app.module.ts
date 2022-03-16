e32656a251bf5e89715378b06f539c5886d2542b
m7mdashrf <m7mdashrf01@gmail.com>
2022-03-16 15:53:31 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { HomeComponent } from './Components/home/home.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { ProductListComponent } from './Components/order/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+    HomeComponent,
+    SidebarComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
