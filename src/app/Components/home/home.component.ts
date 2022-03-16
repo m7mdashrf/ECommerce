1d9265f2458e09810b2af698ef595d53f8781bfc
m7mdashrf <m7mdashrf01@gmail.com>
2022-03-16 20:17:01 +0200

    commit

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreData } from 'src/app/ViewModels/store-data';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  storeInfo : StoreData;
+  isImgShown:boolean = true;
+  constructor() {
+    this.storeInfo = new StoreData('ITI Store','https://picsum.photos/seed/picsum/500/500', ['Cairo', 'Mansoura', 'Alex'] )
+    
+  }
 
   ngOnInit(): void {
+  }
+  toggleImg()
+  {
+    this.isImgShown = !this.isImgShown;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { OrderComponent } from './order/order.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { OrderComponent } from './Components/order/order.component';
 import { HomeComponent } from './Components/home/home.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { ProductListComponent } from './Components/order/product-list/product-list.component';
